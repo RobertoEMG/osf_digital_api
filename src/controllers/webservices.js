f66039b20799455234d8ee97e426dd2b0bd0a68b
RobertoEMG <robertoemg@gmail.com>
2020-11-09 15:39:23 -0600

    Se agregaron parametros de seguimiento

--- src/controllers/webservices.js
+++ src/controllers/webservices.js
@@ -274,7 +274,7 @@ async function BURO(req, res) {
                             }};
                             var soapHeader  = {"Authentication":{"User": oUser,"Password": oPass,"motivoconsulta": "1"}};
                             var argsI = {"dui":oNoDUI, "nit":oNoNIT};
-                            var argsT = {"tipo":"PRD", "docDUI":oNoDUI, "docNIT":oNoNIT};
+                            var argsT = {"tipo":"PRD", "docDUI":oNoDUI, "docNIT":oNoNIT, "origen":"Optima Digital", "idUserOrg":idusers};
 
                             if (tipoburo == 'E') {
                                 soap.createClient(oUrl, function(err, client) {
@@ -477,7 +477,7 @@ async function BURO_PARAMS(req, res) {
                 var vDUI = (dui.substr(0, 8) + '-' + dui.substr(8, 1));
                 var vNIT = (nit.substr(0, 4) + '-' + nit.substr(4, 6) + '-' + nit.substr(10, 3) + '-' + nit.substr(13, 1));
                 var argsI = {"dui":vDUI, "nit":vNIT};
-                var argsT = {"tipo":"PRD", "docDUI":vDUI, "docNIT":vNIT};
+                var argsT = {"tipo":"PRD", "docDUI":vDUI, "docNIT":vNIT, "origen":"Optima Digital", "idUserOrg":idusers};
 
                 if (tipoburo == 'E') {
                     soap.createClient(oUrl, function(err, client) {
